fc6863e5cd58690287e23381af32320883c85bf5
Bela Ban <belaban@yahoo.com>
2015-02-05 18:31:42 +0100

    Set bind_addr to 127.0.0.1 by default

--- bin/demo.sh
+++ bin/demo.sh
@@ -8,7 +8,8 @@
 #BIND_ADDR=site_local
 
 ################# CHANGE THIS ##############################
-BIND_ADDR=match-address:192.168.1.*
+#BIND_ADDR=match-address:192.168.1.*
+BIND_ADDR=127.0.0.1
 ############################################################
 
 MCAST_ADDR=232.5.5.5
