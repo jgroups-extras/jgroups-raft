acf714f1ee0b61e51cabf39fdc3f38de718f2e4a
Jose Bolina <jbolina@redhat.com>
2025-04-12 18:01:19 -0300

    Move test packages
    * Moving test package to match src files. We can test package-private
      code.
    * Try to restrict GHA report only once.
    
    # Conflicts:
    #	src/test/java/org/jgroups/protocols.raft/AppendEntriesTest.java
    #	src/test/java/org/jgroups/protocols.raft/DynamicMembershipTest.java
    #	src/test/java/org/jgroups/protocols.raft/LogEntriesTest.java
    #	src/test/java/org/jgroups/protocols.raft/MergeTest.java
    #	src/test/java/org/jgroups/protocols.raft/PartialConnectivityTest.java
    #	src/test/java/org/jgroups/protocols.raft/RaftHeaderTest.java
    #	src/test/java/org/jgroups/protocols.raft/RaftTest.java
    #	src/test/java/org/jgroups/protocols.raft/SynchronousTests.java
    #	src/test/java/org/jgroups/protocols.raft/election/DelayedElectedLeaderMessageTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/DetermineLeaderBreakdownTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/ElectionsTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/LeaderLeavingTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/NetworkPartitionElectionTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/SyncElectionTests.java
    #	src/test/java/org/jgroups/protocols.raft/election/SyncElectionWithRestrictionTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/SyncLeaderCrashTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/ViewChangeElectionTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/VoteTest.java
    #	src/test/java/org/jgroups/protocols.raft/election/VotingThreadBreakdownTest.java
    #	src/test/java/org/jgroups/raft/CommitTableTest.java
    #	src/test/java/org/jgroups/raft/LogTest.java
    #	src/test/java/org/jgroups/raft/RequestTableTest.java
    #	src/test/java/org/jgroups/raft/TimeoutTest.java
    #	src/test/java/org/jgroups/raft/UtilsTest.java
    #	src/test/java/org/jgroups/raft/blocks/CounterTest.java
    #	src/test/java/org/jgroups/raft/utils/ArrayRingBufferTest.java
    #	src/test/java/org/jgroups/tests/CompletableFutureTest.java
    #	src/test/java/org/jgroups/tests/LongHelperTest.java

---
+++ src/test/java/org/jgroups/protocols.raft/election/DelayedElectedLeaderMessageTest.java
@@ -0,0 +1,106 @@
+package org.jgroups.protocols.raft.election;
+
+import org.jgroups.Global;
+import org.jgroups.Header;
+import org.jgroups.View;
+import org.jgroups.raft.testfwk.BlockingMessageInterceptor;
+import org.jgroups.raft.testfwk.RaftCluster;
+import org.jgroups.raft.tests.harness.BaseRaftElectionTest;
+import org.jgroups.raft.tests.harness.RaftAssertion;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BooleanSupplier;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.jgroups.raft.testfwk.RaftTestUtils.eventually;
+import static org.jgroups.raft.tests.harness.BaseRaftElectionTest.ALL_ELECTION_CLASSES_PROVIDER;
+
+@Test(groups = Global.FUNCTIONAL, singleThreaded = true, dataProvider = ALL_ELECTION_CLASSES_PROVIDER)
+public class DelayedElectedLeaderMessageTest extends BaseRaftElectionTest.ClusterBased<RaftCluster> {
+
+    /**
+     * Use blocking interceptor to capture LeaderElected.
+     * Install complete view to succeed in the election process.
+     * While the message is blocked, remove the majority of nodes.
+     * After new view installed, remove the interceptor.
+     * The node should not install the leader.
+     */
+
+    private static final byte[] BUF = {};
+
+    {
+        createManually = true;
+    }
+
+    @AfterMethod
+    protected void destroy() throws Exception {
+        destroyCluster();
+    }
+
+    public void testQuorumLostAfterMessageSent(Class<?> ignore) throws Exception {
+        long viewId = 0;
+        withClusterSize(5);
+        createCluster();
+
+        // Create complete view to successfully elect a node.
+        // Use a cluster of 5 nodes.
+        View v1 = createView(viewId++, 0, 1, 2, 3, 4);
+
+        // Run asynchronously to allow the voting thread to stop.
+        cluster.async(true);
+
+        // Intercept the first `LeaderElected` message.
+        AtomicBoolean onlyOnce = new AtomicBoolean(true);
+        BlockingMessageInterceptor interceptor = cluster.addCommandInterceptor(m -> {
+            for (Map.Entry<Short, Header> h : m.getHeaders().entrySet()) {
+                if (h.getValue() instanceof LeaderElected && onlyOnce.getAndSet(false)) {
+                    // Assert the coordinator was elected.
+                    LeaderElected le = (LeaderElected) h.getValue();
+                    assertThat(le.leader()).isEqualTo(address(0));
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        // Install view and elect the coordinator.
+        cluster.handleView(v1);
+
+        // Intercept the leader elected message.
+        System.out.println("-- wait command intercept");
+        assertThat(eventually(() -> interceptor.numberOfBlockedMessages() > 0, 10, TimeUnit.SECONDS)).isTrue();
+
+        // Install the new view while the LeaderElected is in-flight.
+        // The new view does not have a majority.
+        System.out.println("-- install new view without majority");
+        View v2 = createView(viewId++, 0, 1);
+        cluster.handleView(v2);
+
+        // Release the leader elected message.
+        // The node should not install the new leader.
+        System.out.println("-- release leader elected message");
+        interceptor.releaseNext();
+        interceptor.assertNoBlockedMessages();
+
+        // Make sure the leader stays null for the whole time.
+        BooleanSupplier bs = () -> Arrays.stream(rafts())
+                .anyMatch(r -> r.leader() != null);
+        assertThat(eventually(bs, 3, TimeUnit.SECONDS))
+                .withFailMessage(this::dumpLeaderAndTerms)
+                .isFalse();
+
+        assertThat(bs.getAsBoolean()).isFalse();
+        RaftAssertion.assertLeaderlessOperationThrows(() -> raft(0).set(BUF, 0, 0));
+    }
+
+    @Override
+    protected RaftCluster createNewMockCluster() {
+        return new RaftCluster();
+    }
+}
---
+++ src/test/java/org/jgroups/protocols.raft/election/VotingThreadBreakdownTest.java
@@ -0,0 +1,141 @@
+package org.jgroups.protocols.raft.election;
+
+import org.jgroups.Global;
+import org.jgroups.View;
+import org.jgroups.protocols.raft.ELECTION;
+import org.jgroups.protocols.raft.RAFT;
+import org.jgroups.raft.testfwk.RaftCluster;
+import org.jgroups.tests.harness.BaseRaftElectionTest;
+import org.jgroups.tests.harness.CheckPoint;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test(groups = Global.FUNCTIONAL, singleThreaded = true, dataProvider = "voting-thread-checkpoint")
+public class VotingThreadBreakdownTest extends BaseRaftElectionTest.ClusterBased<RaftCluster> {
+
+    private CheckPoint checkPoint;
+
+    {
+        createManually = true;
+        checkPoint = new CheckPoint();
+    }
+
+    @AfterMethod
+    protected void destroy() throws Exception {
+        destroyCluster();
+        checkPoint = new CheckPoint();
+    }
+
+    public void testMajorityLostAndGained(Class<?> ignore) throws Exception {
+        long viewId = 0;
+        withClusterSize(3);
+        createCluster();
+
+        // Disable checkpoint for the other nodes.
+        triggerForever("%s_STOP_METHOD_IN_WAIT", 1, 2);
+        triggerForever("%s_START_METHOD_IN_WAIT", 1, 2);
+
+        // Install view with majority to trigger the election mechanism.
+        View v1 = createView(viewId++, 0, 1, 2);
+        System.out.println("-- installing first view with all members");
+        CompletableFuture<Void> f1 = async(() -> cluster.handleView(v1));
+
+        // New view starts the voting thread.
+        checkPoint.awaitStrict("A_START_METHOD_IN", 100, TimeUnit.SECONDS);
+
+        // Install new view without majority.
+        View v2 = createView(viewId++, 0);
+        CompletableFuture<Void> f2 = async(() -> cluster.handleView(v2));
+        checkPoint.awaitStrict("A_STOP_METHOD_IN", 100, TimeUnit.SECONDS);
+        checkPoint.trigger("A_STOP_METHOD_IN_WAIT");
+
+        f2.get(10, TimeUnit.SECONDS);
+
+        // Let the original start voting thread run.
+        // Trigger forever so subsequent starts can happen automatically without waiting.
+        checkPoint.triggerForever("A_START_METHOD_IN_WAIT");
+        f1.get(10, TimeUnit.SECONDS);
+
+        // It will verify the majority is lost and try stop the voting thread.
+        // We install the new view while the coordinator is trying to stop the voting thread.
+        //checkPoint.awaitStrict("A_STOP_METHOD_IN", 10, TimeUnit.SECONDS);
+        View v3 = createView(viewId++, 0, 1, 2);
+
+        System.out.printf("-- installing complete view: %s%n", v3);
+        cluster.add(node(1).getAddress(), node(1));
+        cluster.add(node(2).getAddress(), node(2));
+        CompletableFuture<Void> f3 = async(() -> cluster.handleView(v3));
+
+        // Let the voting thread stop.
+        // Trigger forever so the election can proceed without waiting.
+        checkPoint.triggerForever("A_STOP_METHOD_IN_WAIT");
+
+        // A leader should still be elected.
+        waitUntilLeaderElected(10_000, 0, 1, 2);
+        f3.get(10, TimeUnit.SECONDS);
+    }
+
+    private void triggerForever(String template, int ... indexes) {
+        for (int index : indexes) {
+            RAFT r = raft(index);
+            if (r == null) continue;
+
+            checkPoint.triggerForever(String.format(template, r.raftId()));
+        }
+    }
+
+    @Override
+    protected RaftCluster createNewMockCluster() {
+        return new RaftCluster();
+    }
+
+    @Override
+    protected BaseElection createNewElection() throws Exception {
+        BaseElection be = super.createNewElection();
+        ((VotingThreadCheckpoint) be).checkPoint = checkPoint;
+        return be;
+    }
+
+    @DataProvider(name = "voting-thread-checkpoint")
+    protected Object[][] dataProvider() {
+        return new Object[][] {
+                { VotingThreadCheckpoint.class }
+        };
+    }
+
+    public static class VotingThreadCheckpoint extends ELECTION {
+        private CheckPoint checkPoint;
+
+        public VotingThreadCheckpoint() {
+            setId(ELECTION_ID);
+        }
+
+        @Override
+        public BaseElection stopVotingThread() {
+            checkPoint.trigger(String.format("%s_STOP_METHOD_IN", raft.raftId()));
+            try {
+                checkPoint.awaitStrict(String.format("%s_STOP_METHOD_IN_WAIT", raft.raftId()), 10, TimeUnit.SECONDS);
+            } catch (InterruptedException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+            return super.stopVotingThread();
+        }
+
+        @Override
+        public BaseElection startVotingThread() {
+            checkPoint.trigger(String.format("%s_START_METHOD_IN", raft.raftId()));
+            try {
+                checkPoint.awaitStrict(String.format("%s_START_METHOD_IN_WAIT", raft.raftId()), 10, TimeUnit.SECONDS);
+            } catch (InterruptedException | TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+            return super.startVotingThread();
+        }
+    }
+}
